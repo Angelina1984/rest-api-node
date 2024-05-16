0fd70b5775fa456b51bf89d6c092de9276555a48
ina <rajna_ina@yahoo.com>
2024-05-16 10:26:08 -0700

    Add jsdocs

--- model/User.js
+++ model/User.js
@@ -1,3 +1,16 @@
+/**
+ * User.js
+ *
+ * This file defines the User model schema and exports the User model.
+ * Specifies the name field with type String and required constraint.
+ * Specifies the email field with type String (optional).
+ * Specifies the age field with type Number (optional).
+ * Creates the User model based on the userSchema.
+ * Exports the User model to be used in other parts of the application.
+ *
+ * @author Ikonija Lopez
+ * @version 1.0
+ */
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
